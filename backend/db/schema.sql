467ef7ee2765f45803cd8609c23430c20cfa0318
MohammedAlayed <alayedmohammed0@gmail.com>
2022-01-31 21:08:40 +0200

    Create products  table

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -21,5 +21,17 @@ CREATE TABLE users(
     PRIMARY KEY (id)
 );
 
+CREATE TABLE products(
+id INT AUTO_INCREMENT NOT NULL,
+name: VARCHAR(255),
+type: VARCHAR(255),
+image: VARCHAR(255),
+brand: VARCHAR(255),
+description: VARCHAR(255),
+price: INT(7),
+PRIMARY KEY (id)
+);
+
+
 
 
