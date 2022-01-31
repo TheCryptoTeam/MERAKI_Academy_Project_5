356b850886181329147b5f71270609660dabe852
OmarHamdann <omar.haamdan@gmail.com>
2022-01-31 14:18:13 +0200

    Starter Code

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+
+//===============================================================
+
+const App = () => {
+  return (
+    <div className="App">
+     
+    </div>
+  );
+};
+
+export default App;
