8f013707388b6ee6b6a9d0c87027b4db12eecdec
MohammedAlayed <alayedmohammed0@gmail.com>
2022-02-01 19:26:32 +0200

    getMyCart function and route are done

--- backend/routes/carts.js
+++ backend/routes/carts.js
@@ -1,10 +1,12 @@
 const express = require("express");
 
-const { addTOCart } = require("../controllers/carts");
+const { addTOCart , getMyCart} = require("../controllers/carts");
 const authentication =require("../middlewares/authentication")
 
 const  cartsRouter = express.Router();
 
 cartsRouter.post("/:id",authentication, addTOCart);
+cartsRouter.get("/",getMyCart)
+
 
 module.exports =  cartsRouter;
