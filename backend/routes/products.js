2eabf0276b53f52103e727cb2e6fc9a34d240b9d
yazan hawamdeh <hawamdey@gmail.com>
2022-02-01 16:50:16 +0200

    getProductByName

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,10 +1,12 @@
 const express = require("express");
- const{createNewProduct, getAllProducts}=require("../controllers/products")
+ const{createNewProduct, getAllProducts, getProductByName}=require("../controllers/products")
  
 
 const productsRouter=express.Router();
 
 productsRouter.post("/",createNewProduct)
 productsRouter.get("/",getAllProducts)
+productsRouter.get("/search_1",getProductByName)
+
 
 module.exports=productsRouter
