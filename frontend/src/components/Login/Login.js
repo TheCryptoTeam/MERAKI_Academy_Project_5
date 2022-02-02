4de8ea29dc921d89c2ec02a50bf8027191450e8a
OmarHamdann <omar.haamdan@gmail.com>
2022-02-02 12:55:28 +0200

    create Login component

---
+++ frontend/src/components/Login/Login.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const Login = ({ loggedin }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [token, setToken] = useState("");
+  const [message, setmessage] = useState("");
+
+  const navigate = useNavigate();
+  const body = {
+    email: email,
+    password: password,
+  };
+  const register = () => {
+    navigate("/register");
+  };
+  const loginUser = () => {
+    //show backend server
+    axios
+      //send data from body object
+      .post("http://localhost:5000/login", body)
+      .then((result) => {
+        setToken(result.data.token);
+        localStorage.setItem("userToken", result.data.token);
+        loggedin(true);
+        navigate("/home");
+      })
+      .catch((err) => {
+        //if error
+
+        setmessage(err.response.data.message);
+      });
+  };
+  return (
+    <div className="authentication">
+      
+      <div className="authentication-form">
+        
+        <h1>Login</h1>
+      
+        <div className="authentication-inputs">
+          <label>Email</label>
+          <input
+            type="email"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            placeholder="Enter Email Address"
+            required=""
+          />
+          <label>Password</label>
+          <input
+            type="password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            placeholder="Enter Password"
+            required=""
+          />
+          <button onClick={loginUser}>Login</button>
+          {message ? <p className="error">{message}</p> : <></>}
+        </div>
+
+        <p>
+          You don't have an account?{" "}
+          <a className="no-account" onClick={register}>
+            Register
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
+export default Login;
