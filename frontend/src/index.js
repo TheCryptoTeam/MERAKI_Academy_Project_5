3e2dde9b9dd4eed211c2d5930a945715ebe6bc69
MohammedAlayed <alayedmohammed0@gmail.com>
2022-02-02 18:02:13 +0200

    reduxProvider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
+import { Provider } from "react-redux";
+import store from "./reducer/index";
+
 
 ReactDOM.render(
   <Router>
+    <Provider store={store}>
     <App />
+    </Provider>
   </Router>,
   document.getElementById("root")
 );
