c3d1f9c016d5fee00696e0fb88e66efb70a9d659
yazan hawamdeh <hawamdey@gmail.com>
2022-02-02 10:58:45 +0200

    get product by id

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,5 +1,7 @@
 const express = require("express");
- const{createNewProduct, getAllProducts, getProductByName, getProductByType, getProductByBrand, updateProductById, deleteProductById}=require("../controllers/products")
+ const{createNewProduct, getAllProducts, getProductByName,
+     getProductByType, getProductByBrand, updateProductById,
+      deleteProductById, getProductById}=require("../controllers/products")
  
 
 const productsRouter=express.Router();
@@ -7,9 +9,11 @@ const productsRouter=express.Router();
 productsRouter.post("/",createNewProduct)
 productsRouter.get("/",getAllProducts)
 
+productsRouter.get("/search",getProductById)
 productsRouter.get("/search_1",getProductByName)
 productsRouter.get("/search_2",getProductByType)
 productsRouter.get("/search_3",getProductByBrand)
+
 productsRouter.put("/:id", updateProductById);
 productsRouter.delete("/:id",deleteProductById)
 
