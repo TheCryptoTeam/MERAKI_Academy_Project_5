d17fc4be6499ef20a9123abbdc36c6dafa1930ac
yazan hawamdeh <hawamdey@gmail.com>
2022-02-03 16:10:37 +0200

    edit search

--- frontend/src/components/Search/Search.js
+++ frontend/src/components/Search/Search.js
@@ -1,27 +1,74 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
+import { setproducts, addproduct, updateproductById, deleteProductById } from "../../reducer/products";
 
 
 
 
-const Search=()=>{
+const Search = () => {
 
-const [search,setSearch]=useState("")
+    const [search, setSearch] = useState("")
+    const dispatch = useDispatch();
+    const [show, setShow] = useState(0);
+    const state = useSelector((state) => {
+        return { token: state.loginReducer.token, products: state.productsReducer.products, isLoggedIn: state.loginReducer.isLoggedIn };
+
+    })
 
 
-    const getProductByName = async (name) => {
+
+    const getProductByName = async () => {
         await axios
-            .get(`http://localhost:5000/products/search_name?name=${name}`)
+            .get(`http://localhost:5000/products/search_name?name=${search}`)
             .then((res) => {
-                dispatch(setproducts(res.data.result))
+                console.log(res.data.products);if (!res.data.products[0]) {
+                    setShow(2)
+                }else{ dispatch(setproducts(res.data.products))
+                    setShow(1)}
 
-                // setShow(true)
+               
             })
             .catch((err) => {
-               throw err
+
+                throw err
             });
     };
+
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////
+    //
+
+
+    // useEffect(() => {
+    //     getProductByName();
+    //   }, []);
+
+
+    // const render=()=>{
+
+    //     return(
+    //         <div>
+    //                   {
+    //                 show && state.products.map((product, index) => {
+    //                     return <div key={index} className="products">
+    //                         <div className="product">
+    //                             <p>name:{product.name}</p>
+    //                             <p>price:{product.price}</p>
+    //                             <button className="add">add to cart</button>
+    //                             <button className="add">add to wishList</button>
+    //                         </div><br/>
+    //                     </div>
+
+    //                 })
+    //             }
+
+    //         </div>
+
+    //     )
+
+    // }
+
 
 
 
@@ -29,10 +76,43 @@ const [search,setSearch]=useState("")
     return (
         <div>
             <div>
-                <input className="search" placeholder="search..."/>
-                <button onClick={(e)=>{getProductByName(e.target.value);console.log(e.target.value);}}>search</button>
+                <input className="search" placeholder="search..."
+                    onChange={(event) => { setSearch(event.target.value); }} onKeyPress={(e) => {
+                        console.log(e.key);
+                        
+                        if (e.key === 'Enter') {
+
+                            console.log(show);
+                            getProductByName()
+                        }
+                    }} />
+
+
+                <div>
+
+
+                    {
+                        show==1 ? (state.products.map((product, index) => {
+                            return <div key={index} className="products">
+                                <div className="product">
+                                    <p>name:{product.name}</p>
+                                    <p>price:{product.price}</p>
+                                    <button className="add">add to cart</button>
+                                    <button className="add">add to wishList</button>
+                                </div><br />
+                            </div>
+
+                        })) : show == 2 ? <><p>Not found !!</p></> : <></>
+
+                    }
+
+                </div>
+
+
+
             </div>
         </div>
+
     )
 }
 
