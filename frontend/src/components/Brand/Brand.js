57d1d39741c26f799e66343d6ea6a3172015ba38
MohammedAlayed <alayedmohammed0@gmail.com>
2022-02-05 13:05:16 +0200

    wishList function in brand comp..

--- frontend/src/components/Brand/Brand.js
+++ frontend/src/components/Brand/Brand.js
@@ -2,12 +2,18 @@ import React from "react";
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
-
+import { useSelector } from "react-redux";
 const Brand = () => {
+  
   const [products, setProducts] = useState([]);
   const [show, setShow] = useState(false);
   const { brand } = useParams();
   const navigate = useNavigate();
+  
+    const state = useSelector((state) => {
+        return { token: state.loginReducer.token};
+
+    })
 
   const getByBrand = () => {
     axios
@@ -22,6 +28,37 @@ const Brand = () => {
         console.log(err);
       });
   };
+  //=======================================
+  const addToCart = async (id) => {
+    const headers = {
+        Authorization: `Bearer ${state.token}`,
+    };
+    let quantity = 1;
+    await axios.post(`http://localhost:5000/carts/${id}`, { quantity }, { headers })
+        .then((res) => {
+            
+        })
+}
+
+  //======================================
+
+  //=======================================
+  const addToWishList = async (id) => {
+    console.log(state.token);
+    const headers = {
+        Authorization: `Bearer ${state.token}`,
+    };
+
+    await axios.post(`http://localhost:5000/wishList/${id}`, {},{ headers })
+        .then((res) => {
+           
+        })
+        .catch(err => {
+            console.log(err);
+        })
+}
+
+  //======================================
 
   useEffect(() => {
     getByBrand();
@@ -42,8 +79,8 @@ const Brand = () => {
                   />
                   <p>name:{product.name}</p>
                   <p>price:{product.price}</p>
-                  <button className="add">add to cart</button>
-                  <button className="add">add to wishList</button>
+                  <button className="add" onClick={() => { addToCart(product.id) }}>add to cart</button>
+                  <button className="add" onClick={() => { addToWishList(product.id) }}>add to wishList</button>
                 </div>
                 <br />
               </div>
