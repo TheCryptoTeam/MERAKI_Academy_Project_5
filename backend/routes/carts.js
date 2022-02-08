50454e859fea1d0ac35cef45e72bd5baa8bf97a8
MohammedAlayed <alayedmohammed0@gmail.com>
2022-02-08 22:26:48 +0200

    create updateCartById

--- backend/routes/carts.js
+++ backend/routes/carts.js
@@ -4,6 +4,7 @@ const {
   addTOCart,
   getMyCart,
   deleteFromMyCart,
+  updateCartById
 } = require("../controllers/carts");
 const authentication = require("../middlewares/authentication");
 
@@ -12,5 +13,6 @@ const cartsRouter = express.Router();
 cartsRouter.post("/:id", authentication, addTOCart);
 cartsRouter.get("/",authentication, getMyCart);
 cartsRouter.delete("/:id", deleteFromMyCart);
+cartsRouter.put("/:id",updateCartById);
 
 module.exports = cartsRouter;
