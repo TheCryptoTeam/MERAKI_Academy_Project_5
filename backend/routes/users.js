bb5745a1020067508ed52415e9477c752be21861
MohammedAlayed <alayedmohammed0@gmail.com>
2022-02-09 21:59:55 +0200

    getAllUsers backend done

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,12 +1,13 @@
 const express = require("express");
 
-const { createNewAuthor, getAllUsers,deleteUserById } = require("../controllers/users");
+const { createNewAuthor, getAllUsers,deleteUserById,getUsersNoLimit } = require("../controllers/users");
 
 const usersRouter = express.Router();
 
 usersRouter.post("/", createNewAuthor);
 
 usersRouter.get("/", getAllUsers);
+usersRouter.get("/AllUsers", getUsersNoLimit);
 usersRouter.delete("/:id", deleteUserById);
 
 
