a8c3fbbe5771667db6973318f0f6fb05ae875e1c
MohammedAlayed <alayedmohammed0@gmail.com>
2022-02-10 02:45:00 +0200

    add charts

--- frontend/src/components/ProductsTable/ProductsTable.js
+++ frontend/src/components/ProductsTable/ProductsTable.js
@@ -12,6 +12,25 @@ import {
 import "./ProductsTable.css";
 
 import { RiDeleteBinLine } from "react-icons/ri";
+import  { PureComponent } from 'react';
+
+import {
+  ComposedChart,
+  Line,
+  Area,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  Scatter,
+  ResponsiveContainer,
+} from 'recharts';
+
+
+
+
 const ProductsTable = () => {
   ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -122,7 +141,94 @@ const ProductsTable = () => {
   };
 
   ////////////////////////////////////////////////////////////////return
+//==============================
+const data = [
+  {
+    name: 'Laptop',
+    uv: 800,
+    pv: 1400,
+    amt: 1400,
+    cnt: 800,
+  },
+  {
+    name: 'Mobile',
+    uv: 700,
+    pv: 1250,
+    amt: 1250,
+    cnt: 700,
+  },
+  {
+    name: 'TV',
+    uv: 600,
+    pv: 700,
+    amt: 700,
+    cnt: 600,
+  },
+  {
+    name: 'Wathces',
+    uv: 900,
+    pv: 1200,
+    amt: 1200,
+    cnt: 900,
+  },
+  {
+    name: 'Camera',
+    uv: 650,
+    pv: 900,
+    amt: 900,
+    cnt: 650,
+  },
+ 
+];
 
+//=============================================
+const data2 = [
+  {
+    name: 'Sep',
+   
+    pv: 1100,
+    amt: 1100,
+  
+  },
+  {
+    name: 'Oct',
+   
+    pv: 950,
+    amt: 950,
+    
+  },
+  {
+    name: 'Nov',
+    
+    pv: 1200,
+    amt: 1200,
+    
+  },
+  {
+    name: 'Dec',
+   
+    pv: 1600,
+    amt: 1600,
+    
+  },
+  {
+    name: 'Jan',
+   
+    pv: 1200,
+    amt: 1200,
+    
+  },
+  {
+    name: 'Feb',
+   
+    pv: 700,
+    amt: 700,
+    
+  },
+ 
+];
+
+//=================================
   return (
     <div className="main">
       <div className="side">
@@ -181,8 +287,57 @@ const ProductsTable = () => {
         </div>
       </div>
       <div className="charts">
-        <div></div>
-        <div></div>
+        <div>
+        <ResponsiveContainer width="100%" height="100%">
+        <ComposedChart
+          width={500}
+          height={400}
+          data={data}
+          margin={{
+            top: 20,
+            right: 20,
+            bottom: 20,
+            left: 20,
+          }}
+        >
+          <CartesianGrid stroke="#f5f5f5" />
+          <XAxis dataKey="name" scale="band" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Area type="monotone" dataKey="amt" fill="#8884d8" stroke="#8884d8" />
+          <Bar dataKey="pv" barSize={20} fill="#413ea0" />
+          <Line type="monotone" dataKey="uv" stroke="#ff7300" />
+          <Scatter dataKey="cnt" fill="red" />
+        </ComposedChart>
+      </ResponsiveContainer>
+        </div>
+        <div>
+        <ResponsiveContainer width="100%" height="100%">
+        <ComposedChart
+          width={500}
+          height={400}
+          data={data2}
+          margin={{
+            top: 20,
+            right: 20,
+            bottom: 20,
+            left: 20,
+          }}
+        >
+          <CartesianGrid stroke="#f5f5f5" />
+          <XAxis dataKey="name" scale="band" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Area type="monotone" dataKey="amt" fill="#8884d8" stroke="#8884d8" />
+          <Bar dataKey="pv" barSize={20} fill="#413ea0" />
+          <Line type="monotone" dataKey="uv" stroke="#ff7300" />
+          <Scatter dataKey="cnt" fill="red" />
+        </ComposedChart>
+      </ResponsiveContainer>
+        
+        </div>
       </div>
       <div className="table">
         <div className="tableDiv">
