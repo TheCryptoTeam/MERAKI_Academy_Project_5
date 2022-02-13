5a5947fb1646855f6d3299bffabb71b8b622c155
OmarHamdann <omar.haamdan@gmail.com>
2022-02-13 13:07:38 +0200

    add chat bot

---
+++ frontend/src/chatbot.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+export class Chat extends Component {
+  componentDidMount() {
+    (function (d, m) {
+      var kommunicateSettings = {
+        appId: "12c0d3df85639454753f5e0c42ded5dac",
+        popupWidget: true,
+        automaticChatOpenOnNavigation: true,
+      };
+      var s = document.createElement("script");
+      s.type = "text/javascript";
+      s.async = true;
+      s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+      var h = document.getElementsByTagName("head")[0];
+      h.appendChild(s);
+      window.kommunicate = m;
+      m._globals = kommunicateSettings;
+    })(document, window.kommunicate || {});
+    
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
