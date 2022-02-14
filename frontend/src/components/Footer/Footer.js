56f430cb547cd6d9f44f84bb5cc76eb3f461886f
yazan hawamdeh <hawamdey@gmail.com>
2022-02-14 11:16:29 +0200

    clean footer js

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -5,7 +5,6 @@ import "./Footer.css";
 import { BsFacebook } from 'react-icons/bs';
 import { ImGooglePlus3 } from 'react-icons/im';
 import { FaInstagram } from 'react-icons/fa';
-// import { SMTPClient } from 'emailjs';
 import { useRef, useState } from "react";
 import emailjs from 'emailjs-com';
 import { useDispatch, useSelector } from "react-redux";
@@ -14,7 +13,6 @@ import { init } from '@emailjs/browser';
 init("user_SBk74LXiwtQFn10I8H4vW");
 
 
-// import Logo from './logo.svg';
 
 
 const Footer = () => {
@@ -30,48 +28,22 @@ const Footer = () => {
         };
     });
 
-    // var templateParams = {
-    //     name: 'James',
-    //     notes: 'Check this out!'
-    // };
+
     const sendEmail = (e) => {
         e.preventDefault();
-        emailjs.send("service_6fetmt6","template_i0a03na",{
+        emailjs.send("service_6fetmt6", "template_i0a03na", {
             from_name: localStorage.getItem('userName'),
             to_name: 'Crypto team',
-            message:txtFeedBack,
+            message: txtFeedBack,
             reply_to: "crypto",
-            },'user_SBk74LXiwtQFn10I8H4vW')
+        }, 'user_SBk74LXiwtQFn10I8H4vW')
             .then((response) => {
                 console.log('SUCCESS!', response.status, response.text);
             }, (err) => {
                 console.log('FAILED...', err);
             });
     }
-    // const sendEmail=()=>{
 
-    //     const client = new SMTPClient({
-    //         user: state.token.userName,
-    //         password: state.token.password,
-    //         host: state.token.email,
-    //         ssl: true,
-    //     });
-
-    //     // send the message and get a callback with an error or details of the message that was sent
-    //     client.send(
-    //         {
-    //             text: txtFeedBack,
-    //             from: state.token.username,
-    //             to:'<Crypto@gmail.com>',
-    //             //  'someone <someone@your-email.com>, another <another@your-email.com>',
-    //             // cc: 'else <else@your-email.com>',
-    //             // subject: 'testing emailjs',
-    //         },
-    //         (err, message) => {
-    //             console.log(err || message);
-    //         }
-    //     );
-    // }
 
 
     return (
@@ -80,7 +52,6 @@ const Footer = () => {
             <div className="footer">
                 <div>
 
-                    {/* <img src={Logo} alt="logo" className="logo" /> */}
                     <h1>Crypto</h1><br />
                     <p>Get 10% discount with notified about<br /> the latest news and updates. </p>
                 </div>
