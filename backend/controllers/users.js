d3f3a3eeef84fe82cd793b80431097497ff9122b
OmarHamdann <omar.haamdan@gmail.com>
2022-02-14 11:23:34 +0200

    user

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -4,14 +4,14 @@ const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
 const createNewAuthor = async (req, res) => {
-  console.log(req.body);
+  
   const { userName, email, password, role_id } = req.body;
   let encryptedPassword;
   if (password) {
     encryptedPassword = await bcrypt.hash(password, saltRounds);
     
   }
-console.log(encryptedPassword);
+
   const query = `INSERT INTO users ( userName,email, password, role_id) VALUES (?,?,?,?)`;
   const data = [userName, email, encryptedPassword, role_id];
   connection.query(query, data, (err, results) => {
